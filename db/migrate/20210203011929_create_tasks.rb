bc517ec577ac2a9555c4d01025c97369c309d00e
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-03 01:39:26 +0000

    Taskモデル作成

---
+++ db/migrate/20210203011929_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :content
+      t.datetime :deadline
+      t.string :status
+      t.string :priority
+      t.timestamps
+    end
+  end
+end
