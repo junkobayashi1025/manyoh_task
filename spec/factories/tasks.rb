679c8e76f4a6ba4936c8505507a24da8b5ff144a
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-04 09:27:05 +0000

    Model Specの実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'task' }
     content { 'content' }
-    status {'status'}
-    priority {'priority'}
     deadline { Time.current }
+    status {'New'}
+    priority {'low'}
+
   end
 end
