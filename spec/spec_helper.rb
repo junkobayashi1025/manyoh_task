4a38ea44044f1e57d6800dc9ebf5114a6dfd44cf
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-04 03:48:01 +0000

    Rspec実装

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,10 @@ RSpec.configure do |config|
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+    # 「headless_chromeのno-sandboxオプション」を追加する
+   config.before(:each) do |example|
+     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { args: ['no-sandbox'] } if example.metadata[:type] == :system
+  end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
