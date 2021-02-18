7fa02013261555307bdd9617be94a87e8432cde9
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-18 11:31:32 +0000

    seedにlabelデータ記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ User.find_or_create_by!(email: 'admin@example.com') do |user|
   user.password = 'password'
   user.password_confirmation = 'password'
 end
+
+Label.create!(name: 'work')
+Label.create!(name: 'private')
+Label.create!(name: 'other')
