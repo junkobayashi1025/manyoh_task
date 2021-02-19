16ec0b0ca8e21466bd289c7078dbb80ece7df0af
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-19 04:05:02 +0000

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,29 @@ end
 Label.create!(name: 'work')
 Label.create!(name: 'private')
 Label.create!(name: 'other')
+
+7.times do |n|
+  Label.create!(
+    name: "No.#{n + 1}"
+  )
+end
+
+20.times do |n|
+  User.create!(
+    name: "#{n + 1}番さん",
+    email: "user#{n + 1}@example.com",
+    password: "password#{n + 1}"
+  )
+end
+
+20.times do |n|
+  Task.create!(
+    title: "やること#{n + 1}",
+    content: "プログラミング勉強#{n + 1}",
+    deadline: Time.current + (n).days,
+    status: "New",
+    priority: "normal",
+    user_id: 1,
+    label_ids: 2
+  )
+end
