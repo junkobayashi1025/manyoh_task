4b8da01aa444c9b3538cd77c760ba028e00506eb
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-22 05:34:16 +0000

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min
+
 //= require jquery3
 //= require jquery_ujs
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
